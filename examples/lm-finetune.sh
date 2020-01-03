3a0784f89565e785db3ee7f8e3e8859ea2b098bd
Kuang Wei <kuang.wei@strong.io>
2020-01-03 18:52:20 +0000

    switch to mastiff training for shell

--- examples/lm-finetune.sh
+++ examples/lm-finetune.sh
@@ -1,7 +1,9 @@
 export TRAIN_FILE=/home/kuangwei/bark/text-en/notebooks/distil-prototype/data/wikitext-2-raw/wiki.train.raw
+#export TRAIN_FILE=/home/kuangwei/bark/text-en/notebooks/distil-prototype/data/bark-text/conversation/conversation_1-3.txt
 export TEST_FILE=/home/kuangwei/bark/text-en/notebooks/distil-prototype/data/wikitext-2-raw/wiki.test.raw
+#export TEST_FILE=/home/kuangwei/bark/text-en/notebooks/distil-prototype/data/bark-text/conversation/conversation_4-5_vvvsmall.txt
 python run_lm_finetuning.py \
-    --output_dir=output-wiki2 \
+    --output_dir=output-mastiff \
     --model_type=distilbert \
     --model_name_or_path=distilbert-base-uncased \
     --do_train \
